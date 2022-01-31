19843bc15ee0ed5908cc25a8a2e585e1e76ba131
Nataly Silva <mildrednatalys@gmail.com>
2022-01-30 21:06:44 -0600

    Added search input and animations

--- day_7/app.js
+++ day_7/app.js
@@ -1,4 +1,7 @@
 const menuLeft = document.getElementsByClassName('menu-left-icon')
+const menuRigth = document.getElementsByClassName('square-icon')
+const searchBox = document.getElementsByClassName('search-box')
+
 const notifications = document.getElementsByClassName('notifications')
 const menuOptions = document.getElementsByClassName('menu-options')
 
@@ -7,4 +10,9 @@ const openMenu = () => {
 	menuOptions[0].classList.toggle("transition-menu-options")
 }
 
+const search = () => {
+	searchBox[0].classList.toggle("search-transition")
+}
+
 menuLeft[0].addEventListener('click', openMenu, false )
+menuRigth[0].addEventListener('click', search, false )
